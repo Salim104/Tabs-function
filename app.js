f5a22ae5bc710cc264bcd860e83b9424f6797950
salim104 <salimshaban885@gmail.com>
2021-12-26 12:39:33 +0200

    Tabs

---
+++ app.js
@@ -0,0 +1,24 @@
+//selectors 
+const btns= document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about')
+const articles = document.querySelectorAll('.content');
+
+
+about.addEventListener('click',function(e){
+   const id = e.target.dataset.id;
+   if(id){
+        //remove active from other btn
+     btns.forEach(function(btn){
+        btn.classList.remove('active');
+        e.target.classList.add('active');
+     })
+     /// hide all articles
+      articles.forEach(function(art){
+        art.classList.remove('active');
+     })
+     /// matching the ids in the elememt
+     const element = document.getElementById(id);
+     element.classList.add('active')
+   }
+})
+
